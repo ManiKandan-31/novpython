b93b8c79d8d42bde978ada0cf7a05e69c4ce8782
ManiKandan-31 <mani31102002@gmail.com>
2024-12-17 00:00:43 +0530

    Update elif.py

--- elif.py
+++ elif.py
@@ -19,9 +19,9 @@ sum_marks=maths+phy+chem
 per=(sum_marks/total_marks)*100
 print(per)
 if per<35:
-     print('Grade is F')
+       print('Grade is F')
 elif per>35 and per<=50:
-      print('Grade is C')
+       print('Grade is C')
 elif per in range(51,91):
        print('Grade is B')
 elif per in range(91,101):
