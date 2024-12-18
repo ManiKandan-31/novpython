6db17c6bd2b84a3dbca146b1092548b134644975
ManiKandan-31 <mani31102002@gmail.com>
2024-12-18 09:08:02 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,46 @@
+# count=0
+# val='arjun'
+# for x in val:
+#     print(x)
+#     count=count+1
+#     print(count)
+
+
+
+# count=0
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+# print('total value of odd',count)
+# print('total sum of odd',sum)
+
+
+
+
+
+#Sum of odd
+count=0
+sum=0
+for x in range(1,10,2):
+    print(x)
+    count=count+1
+    sum=sum+x
+print('total value of odd',count)
+print('total sum of odd',sum)
+
+
+
+
+
+# product of odd
+count=0
+sum=0
+product=1
+for x in range(1,10,2):
+    print(x)
+    count=count+1
+    sum=sum+x
+    product=product*x
+print('total value of odd',count)
+print('total sum of odd',sum)
+print('total product of odd',product)
